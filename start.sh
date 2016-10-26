bb7c8720e835cfc6acf26f79216b23565745fe2b
reborg <reborg@reborg.net>
2016-10-26 10:37:42 +0100

    too many startup ports

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 name=${1:-devbox}
 docker run -d -it -e "TERM=xterm-256color" -e "DEVBOX=$name" \
--p ${2:-7000-8000:7000-8000} \
+-p ${2:-7000-7100:7000-7100} \
 --name $name -v /Users/reborg/:/Users/reborg/ $name /bin/bash
